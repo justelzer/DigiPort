f8672f11f260935e12bb2f9d2c5a45b94ed2d739
justelzer <justelzer@hotmail.com>
2026-04-16 15:17:47 -0300

    Arquivo novo Tipos

---
+++ tipos.js
@@ -0,0 +1,11 @@
+let nome = "Julia";
+let idade = 26;
+let ativo = true;
+let nota = 10;
+let defeitos = null;
+
+console.log("Nome: ", nome);
+console.log("Idade: ", idade);
+console.log("Ativo: ", ativo);
+console.log("Nota: ", nota);
+console.log("Defeitos: ");
