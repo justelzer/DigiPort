316eaeb0319b9aed7082dc3ebf9308e16cc65324
justelzer <justelzer@hotmail.com>
2026-04-23 14:31:05 -0300

    typeof

--- tipos.js
+++ tipos.js
@@ -1,10 +1,10 @@
-let nome = "Julia";
+const nome = "Julia";
 let idade = 26;
 let ativo = true;
 let nota = 10;
 let defeitos = null;
 
-console.log("Nome: ", nome);
+console.log("Nome: ", nome, typeof nome);
 console.log("Idade: ", idade);
 console.log("Ativo: ", ativo);
 console.log("Nota: ", nota);
