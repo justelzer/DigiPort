ed813afd8cf6c9d082fc91bc8d96458214acddf6
justelzer <justelzer@hotmail.com>
2026-04-23 14:30:24 -0300

    arquivo novo aula 23/04

---
+++ dadosUsuario.js
@@ -0,0 +1,7 @@
+const nome = "Julia";
+const anoNascimento = 1999;
+let anoFuturo = 2034;
+idadeFutura = anoFuturo - anoNascimento;
+
+console.log(`a idade de ${nome} em ${anoFuturo} vai ser ${idadeFutura} anos`);
+
