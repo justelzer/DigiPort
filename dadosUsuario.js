50d589a1ac47435aa60faa11c88e0588725b0e4c
justelzer <justelzer@hotmail.com>
2026-04-23 15:28:15 -0300

    length, conact and slice

--- dadosUsuario.js
+++ dadosUsuario.js
@@ -1,7 +1,20 @@
-const nome = "Julia";
+let nome = "Julia";
 const anoNascimento = 1999;
 let anoFuturo = 2034;
 idadeFutura = anoFuturo - anoNascimento;
 
-console.log(`a idade de ${nome} em ${anoFuturo} vai ser ${idadeFutura} anos`);
+console.log(`A idade de ${nome} em ${anoFuturo} vai ser ${idadeFutura} anos`);
+
+primeiroCaractere = nome[0];
+let tamanho = nome.length;
+ultimoCaractere = nome[nome.length - 1];
+
+console.log(`Primeiro caracter é ${primeiroCaractere}, segundo caracter é ${nome.charAt(1)}, o comprimento é ${tamanho} e o último caracter é ${ultimoCaractere}`);
+
+nome = nome.replace("Julia", "Cookie");
+console.log(nome);
+nome = nome.concat(" Stelzer");
+console.log(nome);
+console.log(nome.slice(0,3));
+
 
