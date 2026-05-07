239da0072c0a0900655e52a231aa3252c519ae53
justelzer <justelzer@hotmail.com>
2026-05-07 14:35:06 -0300

    reorganizing file names

---
+++ aula-07-05.js
@@ -0,0 +1,9 @@
+let saldo = 18000;
+
+if (saldo >= 15000) {
+    console.log("Partiu Disney");
+} else if (saldo >= 5000) {
+    console.log("Bora pra Minas Gerais");
+} else {
+    console.log("Sem férias esse ano");
+}
