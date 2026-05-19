4a198b432b39e4b48ec0887e2226845afd5cb548
justelzer <justelzer@hotmail.com>
2026-05-19 16:04:57 -0300

    push and pop

---
+++ aula-19-05.js
@@ -0,0 +1,24 @@
+// exercicio 1
+let frutasFavoritas = ["Framboesa", "Mirtilo", "Morango", "Cereja", "Manga"];
+for (let i = 0; i < frutasFavoritas.length; i++) {
+    console.log(`Fruta: ${frutasFavoritas[i]} | Posição: ${i}`);
+}
+
+let frutaRemovida = frutasFavoritas.pop();
+console.log(frutaRemovida);
+
+let qtFrutas = frutasFavoritas.push("Pêssego");
+console.log(qtFrutas);
+
+for (i = 0; i < frutasFavoritas.length; i++) {
+    console.log(`Fruta: ${frutasFavoritas[i]} | Posição: ${i}`);
+}
+
+// exercicio 2
+let participantes = ["Júlia", "Vicky", "Ana", "Isabela"];
+for (let i = 0; i < participantes.length; i++) {
+    console.log(`Parabéns por conseguir entrar na competição ${participantes[i]}!`);
+}
+
+// exercicio 3
+
